c21b197e366d4ba0891b324608e8ec24bda96de8
Akash Anand <akashanand@Akashs-Mac-mini.local>
2026-04-05 18:28:42 -0500

    image resize

--- akashanand/app/opengraph-image.tsx
+++ akashanand/app/opengraph-image.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from "next/og";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 export const alt = "Akash Anand portfolio";
 export const size = {
   width: 1200,
